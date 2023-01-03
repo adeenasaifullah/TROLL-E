f9ec190e9da702ea3a863476670fa52b11a2ed03
SafaAbdulHai <s.hai_18625@khi.iba.edu.pk>
2023-01-03 21:16:56 +0500

    signup added

---
+++ lib/views/Login_Signup/SignupInputWrapper.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '/utility.dart';
+
+
+class SignupInputWrapper extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: displayHeight(context) * 0.01),
+          Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: Column(
+                  children: <Widget>[
+                    field(
+                      validateInput: (name) {
+                        if (nameController.text.isEmpty) {
+                          return "* Required";
+                        }
+                        else {
+                          return null;
+                        }
+                      },
+                      // onChanged: (val) {
+                      //   setState(() => email = val);
+                      // },
+                      textController: nameController,
+                      labelText: 'Name',
+                      //hintText: 'email',
+
+                      autoFocus: false,
+                    ),
+                    SizedBox(height: displayHeight(context) * 0.02),
+                    field(
+                      validateInput: (email) {
+                        if (emailController.text.isEmpty) {
+                          return "* Required";
+                        }
+                        if (!RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(email!)
+                        ) {
+                          return "Enter correct email address";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // onChanged: (val) {
+                      //   setState(() => email = val);
+                      // },
+                      textController: emailController,
+                      labelText: 'Email',
+                      //hintText: 'email',
+
+                      autoFocus: false,
+                    ),
+                    SizedBox(height: displayHeight(context) * 0.02),
+                    field(
+                      validateInput: (phone) {
+                        if (phone!.isEmpty) {
+                          return "* Required";
+                        } else if (!RegExp(r'^[0-9]{11}$').hasMatch(phone)) {
+                          //  r'^[0-9]{10}$' pattern plain match number with length 11
+                          return "Enter correct phone number";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // onChanged: (val) {
+                      //   setState(() => email = val);
+                      // },
+                      textController: phoneController,
+                      labelText: 'Phone Number',
+                      //hintText: 'email',
+
+                      autoFocus: false,
+                    ),
+                    SizedBox(height: displayHeight(context) * 0.02),
+                    field(
+                      validateInput: (password) {
+                        if (passwordController.text.isEmpty) {
+                          return "* Required";
+                        }
+                        if (password == null ) {
+                          return "Incorrect Password";
+                        } else {
+                          return null;
+                        }
+                      },
+
+                      // onChanged: (val) {
+                      //   setState(() => password = val);
+                      // },
+                      textController: passwordController,
+                      labelText: 'Password',
+                      //hintText: 'Enter your password',
+
+                      suffixIcon: Icons.visibility_off,
+                      //suffixIcon:  obscureIcon ? Icons.visibility : Icons.visibility_off,
+                      autoFocus: false,
+
+                    ),
+                    SizedBox(height: displayHeight(context) * 0.02),
+                    field(
+                      validateInput: (cpassword) {
+                        if (confirmController.text.isEmpty) {
+                          return "* Required";
+                        }
+                        if (cpassword == null ) {
+                          return "Incorrect Password";
+                        } else {
+                          return null;
+                        }
+                      },
+
+                      // onChanged: (val) {
+                      //   setState(() => password = val);
+                      // },
+                      textController: confirmController,
+                      labelText: 'Confirm Password',
+                      suffixIcon: Icons.visibility_off,
+                      //suffixIcon:  obscureIcon ? Icons.visibility : Icons.visibility_off,
+                      autoFocus: false,
+
+                    ),
+
+                  ]
+              )
+          ),
+
+          SizedBox(height: displayHeight(context) * 0.04),
+          NavButton(
+            buttonText: 'Sign Up',
+            textSize: displayHeight(context) * 0.03,
+            buttonHeight: displayHeight(context) * 0.08,
+            buttonWidth: displayWidth(context) * 0.7,
+            onPressed: ()=> {
+              //do something
+            },
+          ),
+          SizedBox(height: displayHeight(context) * 0.0084),
+          //Roboto_text(textValue: 'Already have an account?', size: 15),
+          TextButton(
+            onPressed: () {
+              // Navigator.of(context).push(MaterialPageRoute(
+              //     builder: (context) => Login()));
+            },
+            child: Text('Already have an account? Login',
+                style: GoogleFonts.roboto(
+                  color: Color(0xFF000000), fontSize: 15, )),
+            //backgroundColor: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}

fb62df0c98fb76f525747748de44e094be5a4b39
MahumFatimaKhan <mfkmahum@gmail.com>
2023-02-05 23:52:12 +0500

    qrcode and homescreen logo

--- lib/views/homescreen/homescreen.dart
+++ lib/views/homescreen/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:troll_e/utility.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:troll_e/views/menu/menu.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,16 +12,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _result = "";
+
+   _scanQR() async {
+    try {
+       await FlutterBarcodeScanner.scanBarcode("#000000", "Cancel", true, ScanMode.QR).then((value)=>setState(()=> _result=value));
+      // setState(() {
+      //   _result = qrResult;
+      // });
+    }
+    catch (ex) {
+      setState(() {
+        _result = "Unknown Error $ex";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Menu(),
-      appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.black),
-          backgroundColor: Colors.white,
-       //   title:  Roboto_heading(textValue: 'Profile', size: 20.sp)
+        extendBodyBehindAppBar:true,
+        drawer: Menu(),
+      appBar:
+      // PreferredSize(
+      //   preferredSize: Size.fromHeight(80.0),
+      //  child:
+        AppBar(
+            toolbarHeight: 80,
+            iconTheme: IconThemeData(color: Colors.black),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            title: Row(mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              Image.asset('Assets/images/TROLL-E-without-tagline.png', fit: BoxFit.cover, height: 180.h,)
+        ])
+         //   title:  Roboto_heading(textValue: 'Profile', size: 20.sp)
 
-      ),
+        ),
+   //   ),
       body: Container(
         width: double.infinity,
 
@@ -38,20 +67,32 @@ class _HomeScreenState extends State<HomeScreen> {
 
         ),
         child: Center(
-          child: Container(
-            height:150.h,
-            width: 150.w,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                color: kPrimaryColor,
-              boxShadow: [
-                BoxShadow(
-                    color: kPrimaryColor,
-                    blurRadius: 80,
-                    spreadRadius: 20
-                )
-              ]
+          child:  GestureDetector(
+            onTap:()  => _scanQR() ,
+            child: Container(
+              height:150.h,
+              width: 150.w,
+              margin: EdgeInsets.all(100.0),
+              decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  shape: BoxShape.circle
+              ),
             ),
+          )
+          // Container(
+          //   height:150.h,
+          //   width: 150.w,
+          //   decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.circular(200),
+          //       color: kPrimaryColor,
+              // boxShadow: [
+              //   BoxShadow(
+              //       color: kPrimaryColor,
+              //       blurRadius: 80,
+              //       spreadRadius: 20
+              //   )
+              // ]
+           // ),
             // child: ElevatedButton(
             //
             //   style: ElevatedButton.styleFrom(
@@ -63,7 +104,7 @@ class _HomeScreenState extends State<HomeScreen> {
             //     child: const Text("HomeScreen"),
             //   onPressed: () {  },
             // ),
-          ),
+         // ),
         ),
       ),
       // bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.profile),

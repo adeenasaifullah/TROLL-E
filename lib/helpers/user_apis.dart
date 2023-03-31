b5413f144231bd9263148734d151af7972e6ac35
SafaAbdulHai <s.hai_18625@khi.iba.edu.pk>
2023-03-31 20:28:00 +0500

    random commit

--- lib/helpers/user_apis.dart
+++ lib/helpers/user_apis.dart
@@ -243,9 +243,6 @@ Future<void> forgotpassword({
         context: context,
         onSuccess: () async{
           print("email sent");
-
-
-
           showSnackBar(
               context,
               'Change password link has been sent to your email.'
